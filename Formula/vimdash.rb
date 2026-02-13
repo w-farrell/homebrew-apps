eb804e60d64dc1bebaefff87cdb751a61caa0aa3
William Farrell <willfarrell22@gmail.com>
2026-02-12 22:47:23 -0600

    fixed url issue

--- Formula/vimdash.rb
+++ Formula/vimdash.rb
@@ -5,20 +5,20 @@ class Vimdash < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://git.willfarrell.me/will-farrell/vimdash/-/releases/v1.0.2/downloads/vimdash_Darwin_arm64.tar.gz"
+      url "https://git.willfarrell.me/willfarrell/vimdash/-/releases/v1.0.2/downloads/vimdash_Darwin_arm64.tar.gz"
 #       sha256 "91a6083496b526ffd8b07c0a0ee8f1e4b0a27d11613befb8b16abd0d6486248e8"
     else
-      url "https://git.willfarrell.me/will-farrell/vimdash/-/releases/v1.0.2/downloads/vimdash_Darwin_x86_64.tar.gz"
+      url "https://git.willfarrell.me/willfarrell/vimdash/-/releases/v1.0.2/downloads/vimdash_Darwin_x86_64.tar.gz"
  #      sha256 "82361895410d2b3a8179cfa3ac8f8fdb71ed4cbef05a1aeb1ddcfacf1e6f1413"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://git.willfarrell.me/will-farrell/vimdash/-/releases/v1.0.2/downloads/vimdash_Linux_arm64.tar.gz"
+      url "https://git.willfarrell.me/willfarrell/vimdash/-/releases/v1.0.2/downloads/vimdash_Linux_arm64.tar.gz"
   #     sha256 "711205a33a52c0804d651bf0451598807c6bb086e25389439d7b4bd1e99b381f5"
     else
-      url "https://git.willfarrell.me/will-farrell/vimdash/-/releases/v1.0.2/downloads/vimdash_Linux_x86_64.tar.gz"
+      url "https://git.willfarrell.me/willfarrell/vimdash/-/releases/v1.0.2/downloads/vimdash_Linux_x86_64.tar.gz"
    #    sha256 "7a65988e9759cc9587fd3abd1a00d70573afde23ea1975c8f24fda616be114be"
     end
   end
